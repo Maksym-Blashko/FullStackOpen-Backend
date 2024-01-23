7a4cd23931526465174c1a9ed6bef97bf6a5c483
Maksym Blashko <blashkoma@gmail.com>
2024-01-23 20:33:49 +0200

    phonebook. database 3.17 (step 5)

--- index.js
+++ index.js
@@ -101,17 +101,31 @@ app.get('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const person = {
+        name: request.body.name,
+        number: request.body.number,
+    }
+
+    Person
+        .findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
 app.delete('/api/persons/:id', (request, response, next) => {
     Person
         .findByIdAndDelete(request.params.id)
-        .then(result => response.status(204).end())
+        .then(() => response.status(204).end())
         .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
     const error = validateRequest(request)
     if (error) {
-        return response.status(400).json(error)
+        return next(error)
     }
 
     const person = new Person({
