33b0fcb17b537b85fc8e6bc0ad325cd409efede5
Maksym Blashko <blashkoma@gmail.com>
2024-01-24 23:45:03 +0200

    phonebook. database 3.22 Lint config

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ const app = express()
 morgan.token('req-body', (req) => JSON.stringify(req.body))
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    switch (error.name) {
-        case 'CastError':
-            return response.status(400).json({ error: 'malformatted id' })
-        case 'ValidationError':
-            return response.status(400).json({ error: error.message })
-    }
+  switch (error.name) {
+  case 'CastError':
+    return response.status(400).json({ error: 'malformatted id' })
+  case 'ValidationError':
+    return response.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(cors())
@@ -32,95 +32,95 @@ let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oc
 
 // Methods
 getCurrentDate = () => {
-    const date = new Date()
-    const dayOfWeek = daysOfWeek[date.getDay()]
-    const day = date.getDate()
-    const year = date.getFullYear()
-    const month = months[date.getMonth()]
-    const hour = date.getHours()
-    const minutes = date.getMinutes()
-    const seconds = date.getSeconds()
-    const timezoneOffset = date.getTimezoneOffset()
+  const date = new Date()
+  const dayOfWeek = daysOfWeek[date.getDay()]
+  const day = date.getDate()
+  const year = date.getFullYear()
+  const month = months[date.getMonth()]
+  const hour = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
+  const timezoneOffset = date.getTimezoneOffset()
 
-    const formattedHour = hour < 10 ? `0${hour}` : hour;
-    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+  const formattedHour = hour < 10 ? `0${hour}` : hour
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes
+  const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds
 
-    const res = `${dayOfWeek} ${month} ${day}, ${year} ${formattedHour}:${formattedMinutes}:${formattedSeconds} UTC${timezoneOffset < 0 ? '+' : '-'}${Math.abs(timezoneOffset / 60)}`;
-    console.log(res)
+  const res = `${dayOfWeek} ${month} ${day}, ${year} ${formattedHour}:${formattedMinutes}:${formattedSeconds} UTC${timezoneOffset < 0 ? '+' : '-'}${Math.abs(timezoneOffset / 60)}`
+  console.log(res)
 
-    return res
+  return res
 }
 
 getInfoPage = (totalPersons) => {
-    const page = `
+  const page = `
         <div>
             <p>Phonebook has info for ${totalPersons} people</p>
             <p>${getCurrentDate()}</p>
         </div>`
 
-    return page
+  return page
 }
 
 // Requests
 app.get('/api/persons', (request, response, next) => {
-    Person
-        .find({})
-        .then(persons => response.json(persons))
-        .catch(error => next(error))
+  Person
+    .find({})
+    .then(persons => response.json(persons))
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response, next) => {
-    Person
-        .countDocuments({})
-        .then(totalPersons => response.send(getInfoPage(totalPersons)))
-        .catch(error => next(error))
+  Person
+    .countDocuments({})
+    .then(totalPersons => response.send(getInfoPage(totalPersons)))
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person
-        .findById(request.params.id)
-        .then(person => {
-            if (person) {
-                response.json(person)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Person
+    .findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const { name, number } = request.body
+  const { name, number } = request.body
 
-    Person
-        .findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
-        .then(updatedPerson => {
-            response.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  Person
+    .findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person
-        .findByIdAndDelete(request.params.id)
-        .then(() => response.status(204).end())
-        .catch(error => next(error))
+  Person
+    .findByIdAndDelete(request.params.id)
+    .then(() => response.status(204).end())
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const person = new Person({
-        name: request.body.name,
-        number: request.body.number,
-    })
+  const person = new Person({
+    name: request.body.name,
+    number: request.body.number,
+  })
 
-    person
-        .save()
-        .then(savedPerson => {
-            console.log(`added ${savedPerson.name} ${savedPerson.number}`)
-            response.json(savedPerson)
-        })
-        .catch(error => next(error))
+  person
+    .save()
+    .then(savedPerson => {
+      console.log(`added ${savedPerson.name} ${savedPerson.number}`)
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.use(errorHandler)
--- models/phoneValidation.js
+++ models/phoneValidation.js
@@ -1,22 +1,22 @@
 module.exports = function(number) {
-    if (number.length < 8) {
-        return false
-    }
+  if (number.length < 8) {
+    return false
+  }
 
-    const parts = number.split('-')
-    if (parts.length !== 2) {
-        return false
-    }
+  const parts = number.split('-')
+  if (parts.length !== 2) {
+    return false
+  }
 
-    const firstPart = parts[0]
-    if (!/^\d{2,3}$/.test(firstPart)) {
-        return false
-    }
+  const firstPart = parts[0]
+  if (!/^\d{2,3}$/.test(firstPart)) {
+    return false
+  }
 
-    const secondPart = parts[1]
-    if (!/^\d+$/.test(secondPart)) {
-        return false
-    }
+  const secondPart = parts[1]
+  if (!/^\d+$/.test(secondPart)) {
+    return false
+  }
 
-    return true
+  return true
 }
